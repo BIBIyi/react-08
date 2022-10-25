2f17f92d7266de632bf2877007abe8b71717453f
Emi Zhu <385115337@qq.com>
2022-10-25 17:05:57 +1100

    small project

---
+++ src/components/Users/AddUser.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+export default function AddUser(props) {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log("name", enteredUsername, "age", enteredAge);
+  };
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+}
